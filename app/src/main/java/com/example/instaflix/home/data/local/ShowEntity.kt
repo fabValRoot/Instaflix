1d1afc350265b3ebf6c84bf3f10c48b523f4040c
fabValRoot <fabian.valencia@rootstrap.com>
2024-09-06 16:28:54 -0500

    initial DB configuration

---
+++ app/src/main/java/com/example/instaflix/home/data/local/ShowEntity.kt
@@ -0,0 +1,20 @@
+package com.example.instaflix.home.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ShowEntity (
+    @PrimaryKey val id: Int,
+    val smallImgPath: String,
+    val language: String,
+    val overview: String,
+    val popularity: Double,
+    val largeImgPath: String,
+    val releaseDate: String,
+    val title: String,
+    val voteAverage: Double,
+    val name: String,
+    val category: String,
+    val showType: String,
+)
---
+++ app/src/main/java/com/example/instaflix/home/data/local/ShowsDao.kt
@@ -0,0 +1,27 @@
+package com.example.instaflix.home.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+
+@Dao
+interface ShowsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertShow(
+        show: ShowEntity
+    )
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertListOfShows(
+        show: List<ShowEntity>
+    )
+
+    @Query("SELECT * FROM showentity WHERE id = :id")
+    suspend fun getShowById(id: Int): ShowEntity
+
+    @Query("SELECT * FROM showentity WHERE category = :category AND showType = :showType")
+    suspend fun getShowsByCategoryAndType(category: String, showType: String): List<ShowEntity>
+}
