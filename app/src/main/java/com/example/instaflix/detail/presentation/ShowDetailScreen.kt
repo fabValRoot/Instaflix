2270b9aa8f319a9753f4feca2a6fed753aa510bb
fabValRoot <fabian.valencia@rootstrap.com>
2024-09-07 15:13:48 -0500

    UI fixes on details screen and code cleaning

--- app/src/main/java/com/example/instaflix/detail/presentation/ShowDetailScreen.kt
+++ app/src/main/java/com/example/instaflix/detail/presentation/ShowDetailScreen.kt
@@ -4,38 +4,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
 import com.example.instaflix.home.domain.models.Show
 import com.example.instaflix.util.ShowPosterComposable
 
 
 @Composable
 fun ShowDetailScreen(
-    navController: NavController,
     show: Show,
-    detailState: ShowDetailState
 ) {
-
-    val imgUrl = "https://image.tmdb.org/t/p/w500/${show.largeImgPath}"
-
-    val imgPainter = rememberAsyncImagePainter(
-        model = ImageRequest.Builder(LocalContext.current)
-            .data(imgUrl)
-            .size(coil.size.Size.ORIGINAL)
-            .build()
-    )
 
     Box(
         modifier = Modifier
@@ -49,42 +35,63 @@ fun ShowDetailScreen(
 
             ShowPosterComposable(show = show)
 
-            Text(
-                text = "${show.showType} / ${show.category}",
-                color = MaterialTheme.colorScheme.onBackground,
-                fontSize = 20.sp
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp)
             )
+            {
+                Text(
+                    text = "Title: ${show.title}${show.name}",
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontSize = 22.sp
+                )
 
-            Spacer(modifier = Modifier.width(8.dp))
+                Spacer(modifier = Modifier.height(8.dp))
 
-            Text(
-                text = "${show.title}${show.name}",
-                color = MaterialTheme.colorScheme.onBackground,
-                fontSize = 20.sp
-            )
+                Text(
+                    text = "Show Type: ${show.showType}",
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontSize = 18.sp
+                )
 
-            Spacer(modifier = Modifier.width(8.dp))
+                Spacer(modifier = Modifier.height(8.dp))
 
-            Text(
-                text = show.language,
-                color = MaterialTheme.colorScheme.onBackground,
-                fontSize = 20.sp
-            )
+                Text(
+                    text = "Category: ${show.category}",
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontSize = 18.sp
+                )
 
-            Spacer(modifier = Modifier.width(8.dp))
+                Spacer(modifier = Modifier.height(8.dp))
 
-            Text(
-                text = show.overview,
-                color = MaterialTheme.colorScheme.onBackground,
-                fontSize = 12.sp
-            )
+                Text(
+                    text = "Language: ${show.language}",
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontSize = 14.sp
+                )
 
-            Spacer(modifier = Modifier.width(8.dp))
+                Spacer(modifier = Modifier.height(8.dp))
 
+                Text(
+                    text = "Release Date: ${show.releaseDate}",
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontSize = 14.sp
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(
+                    text = "Overview: ${show.overview}",
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontSize = 12.sp
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+            }
 
         }
 
     }
-
 
 }
